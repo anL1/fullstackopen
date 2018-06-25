13ed97f84812352bc0d4dca88538d001c57e78ff
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-06-25 12:25:02 +0300

    osa4 init & 4.2 done

---
+++ osa4/index.js
@@ -0,0 +1,31 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('connected to database')
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
